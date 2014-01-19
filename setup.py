bea739dd1344ddd2aa9bd3fac70030a10e386dce
Gergely Nagy <algernon@madhouse-project.org>
2014-01-19 15:30:44 +0100

    setup.py: Bump the version to 0.1.0
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="monaxhyd",
-    version="0.0.0",
+    version="0.1.0",
     install_requires = ['hy>=0.9.12'],
     packages=find_packages(exclude=['tests']),
     package_data={
